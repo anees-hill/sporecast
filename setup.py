dcd3cdc92ffc4467194080aae56c7708ed1bc2ac
Sam Anees-Hill <samuel.anees.hill@gmail.com>
2023-04-27 11:57:39 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Short-term forecasting of outdoor airborne fungal spore concentrations in the East Midlands of England.',
+    author='Samuel Anees-Hill',
+    license='BSD-3',
+)
