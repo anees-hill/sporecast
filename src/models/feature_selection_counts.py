ddd4bd4be25afbe054f4db94ea2a80629b54b1fe
anees-hill <sph22@leicester.ac.uk>
2023-06-08 11:33:15 +0100

    Feature selection and modelling additions

--- src/models/feature_selection_counts.py
+++ src/models/feature_selection_counts.py
@@ -6,8 +6,8 @@ import pickle
 import sys
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
-from sklearn.ensemble import RandomForestClassifier
 from boruta import BorutaPy
+from sklearn.ensemble import RandomForestRegressor
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -16,7 +16,8 @@ base_dir = os.getenv('BASE_DIRECTORY')
 base_dir = Path(base_dir)
 
 outcome_selection = "Alternaria"
-use_spore_lags = False
+
+peaks_file = f"predicted_peaks_{outcome_selection}.pkl"
 
 def load_data(dataset_name):
     with open(base_dir/"data"/"interim"/"imputed_engineered_historical.pkl", "rb") as f:
@@ -33,50 +34,57 @@ data = import_data[0]
 date_split = import_data[1] 
 taxa_list = import_data[2] 
 
+with open(base_dir/"data"/"interim"/peaks_file, "rb") as f:
+    imported_data = pickle.load(f)
+    predicted_test_peaks = imported_data["peaks"]
+    use_spore_lags = imported_data["use_spore_lags"]
+
 corrected_taxa_list = [item.replace("Total_fungal_spores", "sum_other_vars") for item in taxa_list]
 corrected_taxa_list = [taxa for taxa in corrected_taxa_list if taxa not in outcome_selection]
-
-# Check for missing data
-missing_data = {}
-for col in data.columns:
-    missing_data.update({col: data[col].isnull().sum()})
-missing_data = {key: value for key, value in missing_data.items() if value != 0}
 
 # Split
 train = data.query("date < @date_split").set_index("date")
 test = data.query("date >= @date_split").set_index("date")
 
-# Remove cols not in use
-train = train.drop(["outcome", "outcome_lag1", "outcome_lag2", "outcome_lag3", "date_lag1", "date_lag2", "date_lag3",  "c_avg_air_temp", "c_min_air_temp", "c_mean_wind_dir", "c_max_gust_dir", "c_avg_cloudcov"], axis=1)
-test = test.drop(["outcome", "outcome_lag1", "outcome_lag2", "outcome_lag3", "date_lag1", "date_lag2", "date_lag3", "c_max_air_temp", "c_max_mean_wind_speed", "c_avg_dewpoint"], axis=1)
+# Make a copy of the relevant outcome for attachment to predicted 'peak' results for assessment purpose
+outcome_counts_test = test["outcome"].copy()
+
+# Remove cols not in use for modelling
+train = train.drop(["outcome_lag1", "outcome_lag2", "outcome_lag3", "date_lag1", "date_lag2", "date_lag3",  "c_avg_air_temp", "c_min_air_temp", "c_mean_wind_dir", "c_max_gust_dir", "c_avg_cloudcov"], axis=1)
+test = test.drop(["outcome_lag1", "outcome_lag2", "outcome_lag3", "date_lag1", "date_lag2", "date_lag3",  "c_avg_air_temp", "c_min_air_temp", "c_mean_wind_dir", "c_max_gust_dir", "c_avg_cloudcov"], axis=1)
 
 if use_spore_lags == False:
     train = train.loc[:, ~train.columns.str.startswith(tuple(corrected_taxa_list))]
     test = test.loc[:, ~test.columns.str.startswith(tuple(corrected_taxa_list))]
 
+# Separate outcome
+train_X = train.drop("outcome", axis=1)
+test_X = test.drop(["outcome", "peak"], axis=1)
 
-# Separate labels
-train_X = train.drop("peak", axis=1)
-test_X = train.drop("peak", axis=1)
+# Make a copy of the outcome
+train_y = train["outcome"].copy()
+test_y = test["outcome"].copy()
 
-train_y = train["peak"].copy()
-test_y = test["peak"].copy()
+# Add predicted peaks to test dataset
+test_X["peak"] = predicted_test_peaks["predictions"]
 
-train_y = train_y.astype(int)
-test_y = train_y.astype(int)
-
-# Separate categorical and numerical columns
+# Separate cat and num cols
 all_cols = train_X.columns.to_list()
-categorical_cols = ["month","site","decade_cat","week_1","year_type_for_doy_lag1","c_mean_wind_dir_class", "c_max_gust_dir_class"]
+categorical_cols = ["peak","month","site","decade_cat","week_1","year_type_for_doy_lag1","c_mean_wind_dir_class", "c_max_gust_dir_class"]
 numerical_cols = [col for col in all_cols if col not in categorical_cols]
 
+# Preprocessing
 preprocessor = ColumnTransformer(transformers=[
     ("cat", OneHotEncoder(), categorical_cols),
     ("num", StandardScaler(), numerical_cols)
 ])
+train_X_preprocessed = preprocessor.fit_transform(train_X)
+test_X_preprocessed = preprocessor.transform(test_X)
 
-preprocessor.fit(train_X)
-train_X_preprocessed = preprocessor.transform(train_X)
+# Get feature names from preprocessing
+ohe_feature_names = preprocessor.named_transformers_['cat'].get_feature_names_out(input_features=categorical_cols)
+num_feature_names = numerical_cols
+feature_names = np.concatenate([ohe_feature_names, num_feature_names])
 
 # Get transformed column names
 column_names = preprocessor.get_feature_names_out()
@@ -85,15 +93,15 @@ column_names = preprocessor.get_feature_names_out()
 train_X_preprocessed_dense = train_X_preprocessed.toarray()
 train_X_preprocessed_df = pd.DataFrame(train_X_preprocessed_dense, columns=column_names.tolist())
 
-# Define Random Forest classifier
-rf = RandomForestClassifier(n_jobs=-1, random_state=42, bootstrap=True, n_estimators=2000, max_depth=16, max_features=8, min_samples_leaf=1, min_samples_split=10)
+# Define Random Forest regressor
+rf = RandomForestRegressor(n_jobs=-1, random_state=42, bootstrap=True, n_estimators=2000, max_depth=16, max_features=8, min_samples_leaf=1, min_samples_split=10)
 # Boruta feature selection method
 feat_selector = BorutaPy(rf, n_estimators='auto', verbose=2, random_state=1)
 
 # Find relevant features
 feat_selector.fit(train_X_preprocessed_df.values, train_y.values)
 
-file_name = f"selected_features_{outcome_selection}_peak.pkl"
+file_name = f"selected_features_{outcome_selection}_counts.pkl"
 
 # Parse and export selected features
 selected_features = train_X_preprocessed_df.columns[feat_selector.support_]
@@ -101,5 +109,4 @@ selected_features_clean = selected_features.str.replace('num__','').str.replace(
 with open(base_dir/"data"/"interim"/file_name, "wb") as f:
     pickle.dump({'selected_features': selected_features_clean,
                  "use_spore_lags": use_spore_lags}, f)
-
 
